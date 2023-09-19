6890b3613c1ffbf0edb7ff7bf0f0db07aa737233
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-09-19 18:00:29 +0200

    Add files via upload

---
+++ get_data.php
@@ -0,0 +1,37 @@
+<?php
+// Connect to the MySQL database
+$mysqli = new mysqli("localhost", "root", "", "drops");
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+// Define the API endpoint
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Perform a SELECT query
+    $result = $mysqli->query("SELECT * FROM color_table");
+
+    // Check if the query was successful
+    if ($result) {
+        $data = [];
+
+        // Fetch data and add it to the $data array
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        // Return the data as JSON
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // Close the result set and the database connection
+        $result->close();
+    } else {
+        // Handle the error
+        echo "Error: " . $mysqli->error;
+    }
+}
+
+// Close the database connection
+$mysqli->close();
