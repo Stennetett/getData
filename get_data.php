b6e07f28e7372f193f21a1ba9495b12985b343dd
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-09-19 19:04:35 +0200

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to the MySQL database
-$mysqli = new mysqli("root", "localhost", "123banan123@", "sys");
+$mysqli = new mysqli("161.35.66.82", "new_username", "rtfl08@R", "sys");
 
 // Check connection
 if ($mysqli->connect_error) {
