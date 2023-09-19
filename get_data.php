58abcab7d6b28bac4a903d63cee9178359b8f637
Stennetett <56960738+Stennetett@users.noreply.github.com>
2023-09-19 18:35:03 +0200

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -1,9 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello PHP</title>
-</head>
-<body>
-    <h1><?php echo "Hello"; ?></h1>
-</body>
-</html>
+<?php
+// Connect to the MySQL database
+$mysqli = new mysqli("161.35.66.82", "localhost", "123banan123@", "sys");
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+// Define the API endpoint
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Perform a SELECT query
+    $result = $mysqli->query("SELECT * FROM day_plan");
+
+    // Check if the query was successful
+    if ($result) {
+        $data = [];
+
+        // Fetch data and add it to the $data array
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        // Return the data as JSON
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // Close the result set and the database connection
+        $result->close();
+    } else {
+        // Handle the error
+        echo "Error: " . $mysqli->error;
+    }
+}
+
+// Close the database connection
+$mysqli->close();
